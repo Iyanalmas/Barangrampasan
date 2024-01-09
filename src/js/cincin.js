0a324bff66ec9c9aaa4ba3bab95caeed8b014ff8
iyanalmas <iyanalmasalbantani@gmail.com>
2024-01-09 19:46:13 +0700

    sipet

---
+++ src/js/cincin.js
@@ -0,0 +1,24 @@
+// JavaScript to toggle mobile menu visibility
+document.getElementById("burger-menu").addEventListener("click", function () {
+  document.getElementById("mobile-menu").classList.toggle("hidden");
+});
+
+function handleButtonClick() {
+  const konfirmasi = confirm("Anda yakin ingin memesan item ini?");
+  if (konfirmasi) {
+    window.open(
+      "https://api.whatsapp.com/send?phone=+6281335358531&text=Hai%20admin%20BarangRampasan%2C%20saya%20mau%20pesan%20Cincin%2C%20apakah%20barangnya%20ready%3F",
+      "_blank"
+    );
+  }
+}
+
+const button = document.getElementById("tombolCheckout");
+const button2 = document.getElementById("tombolCheckout2");
+const button3 = document.getElementById("tombolCheckout3");
+const button4 = document.getElementById("tombolCheckout4");
+
+button.addEventListener("click", handleButtonClick);
+button2.addEventListener("click", handleButtonClick);
+button3.addEventListener("click", handleButtonClick);
+button4.addEventListener("click", handleButtonClick);
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+// JavaScript to toggle mobile menu visibility
+document.getElementById("burger-menu").addEventListener("click", function () {
+  document.getElementById("mobile-menu").classList.toggle("hidden");
+});
